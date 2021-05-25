8a1ffe9ac6e18aea31e1906704b36fe928abc154
RISHABH BANDOONI <bandooni.rishabh99@gmail.com>
2021-05-25 21:01:54 +0530

    app built

---
+++ src/components/ProductFeed.js
@@ -0,0 +1,56 @@
+import Product from "./Product"
+
+function ProductFeed ({ products }) {
+    return (
+        <div className=" grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 
+        md:-mt-48 lg:-mt-70 xl:-mt-96 -mt-14 mx-auto   ">
+         
+{products.slice(0,4)
+.map(({id,title,price,description,category,image}) => (
+<Product 
+key={id}
+id={id}
+title={title}
+price={price}
+description={description}
+category={category}
+image={image}
+/>
+    ))}
+
+<img  src="https://links.papareact.com/dyz" alt="" className=" md:col-span-full mx-auto" />
+
+
+<div className="md:col-span-2">
+{products.slice(4,5)
+.map(({id,title,price,description,category,image}) => (
+<Product 
+key={id}
+id={id}
+title={title}
+price={price}
+description={description}
+category={category}
+image={image}
+/>
+    ))}
+</div>
+{products.slice(5,products.length)
+.map(({id,title,price,description,category,image}) => (
+<Product 
+key={id}
+id={id}
+title={title}
+price={price}
+description={description}
+category={category}
+image={image}
+/>
+    ))}
+     
+        </div>
+        
+      
+    );
+}
+export default ProductFeed;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,20 @@
+// .sidebar {
+//     min-width: 5rem;
+//     width: clamp(80px,10vw,100px);
+//     height: 100vh;
+//     position: fixed;
+//     z-index: 99;
+//     top: 0;
+//     left: 0;
+//     box-shadow: 0 0 1rem rgb(26 26 44 / 5%);
+//     background: #fff;
+//     display: flex;
+//     flex-flow: column nowrap;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 3rem 0;
+// }
+// *, :after, :before {
+//     box-sizing: border-box;
+//     margin: 0;
+// }
